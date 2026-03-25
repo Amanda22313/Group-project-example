1ac2f0b9bf44d9f7f13b41a961454126e7df3e03
Mbuyiselwa Hadebe <hadebema69@gmail.com>
2026-03-25 09:52:36 +0200

    App.py file

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
